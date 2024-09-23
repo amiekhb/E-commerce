fdfea8ce68acd93c80276961dc4169cb9d3cb624
amiekhb <aminaaami0715@gmail.com>
2024-09-23 09:32:11 +0800

    login

--- front-end/src/app/(main)/login/page.tsx
+++ front-end/src/app/(main)/login/page.tsx
@@ -1,9 +1,40 @@
-import React from "react";
+"use client";
 import { Input } from "../../../components/ui/input";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import React, { useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/router";
+import { toast } from "react-toastify";
 
 const Login = () => {
+  const router = useRouter();
+  const [userData, setUserData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const logIn = async () => {
+    const { email, password } = userData;
+
+    try {
+      const response = await axios.post("http://8008/login", {
+        email,
+        password,
+      });
+
+      if (response.status === 200) {
+        toast.success("User successfully signed in", { autoClose: 1000 });
+        const { token } = response.data;
+        localStorage.setItem("token", token);
+
+        router.push("/dashboard");
+      }
+    } catch (error) {
+      console.error("There was an error signing in:", error);
+      toast.error("Failed to sign in. Please try again.");
+    }
+  };
   return (
     <div className=" p-16 m-16">
       <div className="flex flex-col gap-5 text-center p-16 m-16">
